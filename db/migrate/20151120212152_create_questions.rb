529abb4fe02b312ab99e8fd67f8f165c654e5fee
Linus Liang <linus.h.liang@gmail.com>
2015-11-20 16:40:54 -0500

    added choice fields

--- db/migrate/20151120212152_create_questions.rb
+++ db/migrate/20151120212152_create_questions.rb
@@ -3,6 +3,7 @@ class CreateQuestions < ActiveRecord::Migration
     create_table :questions do |t|
       t.string  :title
       t.bool    :free_response
+      t.integer :max_score
       t.timestamps null: false
     end
   end
