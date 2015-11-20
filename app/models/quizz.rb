de4a9640c53c694783f00c1433f31c0041d97806
Linus Liang <linus.h.liang@gmail.com>
2015-11-20 16:28:04 -0500

    added quizz fields

--- app/models/quizz.rb
+++ app/models/quizz.rb
@@ -1,2 +1,3 @@
 class Quizz < ActiveRecord::Base
+  has_many :questions
 end
--- db/migrate/20151120212042_create_quizzs.rb
+++ db/migrate/20151120212042_create_quizzs.rb
@@ -1,7 +1,9 @@
 class CreateQuizzs < ActiveRecord::Migration
   def change
     create_table :quizzs do |t|
-
+      t.string  :title
+      t.string  :subject
+      t.integer :max_score
       t.timestamps null: false
     end
   end
